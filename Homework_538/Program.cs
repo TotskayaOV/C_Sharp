eb7803cd3239f31ebe17989873c2cb89bb013b00
TotskayaOV <olga.totskaya@gmail.com>
2022-10-18 23:32:02 +0700

    Amendmend Homework 538

--- Homework_538/Program.cs
+++ Homework_538/Program.cs
@@ -1,35 +1,72 @@
 ﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
-double[] array = {3, 7, 22, 2, 78};
+int size = GetNumberFromUser("Введите размер массива: ", "Ошибка ввода!");
+int beforedesjat = GetNumberFromUser("Введите количество цифр перед запятой: ", "Ошибка ввода!");
+int afterdesjat = GetNumberFromUser("Введите количество цифр после запятой: ", "Ошибка ввода!");
+Console.Clear();
+double[] array = GetArray(size);      
+double maximumNum = MaxNumber(array);
+double minimumNum = MinNumber(array);
+double subNum = SubtractingNumber(maximumNum, minimumNum);
+Console.Write("[ ");
+PrintArray(array);  
+Console.Write($"] -> {subNum}");
 
-//находим макс и мин
-
-double Max(double arg1, double arg2, double arg3, double arg4, double arg5)
+void PrintArray(double[] array)        //метод вывода массива
 {
-    double result = arg1;
-    if(arg2 > result) result = arg2;
-    if(arg3 > result) result = arg3;
-    if(arg4 > result) result = arg4;
-    if(arg5 > result) result = arg5;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+double[] GetArray(int size)        //метод создания массива
+{
+    double[] res = new double[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().NextDouble();
+        res[i] = res[i] * Math.Pow(10, beforedesjat);
+        res[i] = Math.Round(res[i], afterdesjat);
+    }
+    return res;
+}
+
+int GetNumberFromUser(string message, string errorMessage)  //метод ввода числа
+{
+    while(true)     
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)       
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+double MaxNumber(double[] array)        //метод нахождения максимального
+{
+    double maximumNum = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (maximumNum < array[i]) maximumNum = array[i];       
+    }  
+    return maximumNum;
+}
+
+double MinNumber(double[] array)        //метод нахождения минимального
+{
+    double minimumNum = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (minimumNum > array[i]) minimumNum = array[i];       
+    }  
+    return minimumNum;
+}
+
+double SubtractingNumber(double maximumNum, double minimumNum)  //метод вычитания
+{
+    double result = maximumNum - minimumNum;
     return result;
 }
-double max = Max(array[0], array[1], array[2], array[3], array[4]);
-
-double Min(double arg1, double arg2, double arg3, double arg4, double arg5)
-{
-    double result = arg1;
-    if(arg2 < result) result = arg2;
-    if(arg3 < result) result = arg3;
-    if(arg4 < result) result = arg4;
-    if(arg5 < result) result = arg5;
-    return result;
-}
-double min = Min(array[0], array[1], array[2], array[3], array[4]);
-Console.WriteLine(max);
-Console.WriteLine(min);
-
-//находим разницу
-
-double numbers = max - min;
-
-Console.Write($"{array} -> {numbers}");
